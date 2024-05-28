a1e67f39aee88d66c481c21ab889e5808c85f72c
Johnathan23079 <95047376+Johnathan23079@users.noreply.github.com>
2024-05-28 12:52:21 -0400

    Create Objects.py

---
+++ Classes/Objects.py
@@ -0,0 +1,10 @@
+#fuison360 api
+#object = ExtrudeFeature
+#Application(Fuison) - UI - Design? - Solid - Extrudefeature
+#sketch - sketch line
+class LEAD:
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+apiSeries = (LEAD)
+formsSeries = (LEAD) 
