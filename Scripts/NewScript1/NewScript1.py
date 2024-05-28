e67415bb3d57bcc724d24054e6fd8baf6a121008
Johnathan23079 <95047376+Johnathan23079@users.noreply.github.com>
2024-05-28 12:09:19 -0400

    Update NewScript1.py

--- Scripts/NewScript1/NewScript1.py
+++ Scripts/NewScript1/NewScript1.py
@@ -1,15 +1,21 @@
-#Author-
-#Description-
+#Author-Johnathan Lee
+#Description-Starter Code
 
 import adsk.core, adsk.fusion, adsk.cam, traceback
 
-def run(context):
-    ui = None
-    try:
-        app = adsk.core.Application.get()
-        ui  = app.userInterface
-        ui.messageBox('Hello script')
+# adsk = Autodesk Core Package (Core, Fusion, CAM)
+# traceback = Python Standard Library (traceback)
 
-    except:
+def run(context): # Function that runs on startup of script
+    ui = None  # User Interface
+    try: # Try to run the following code block, most code will be placed here
+        app = adsk.core.Application.get()   # Get the application and assign to the "app" variable 
+
+        ui  = app.userInterface # Get the user interface
+
+        ui.messageBox('Hello script') # Display a message box with the text 'Hello script' that pops up on run
+
+    except: # If an error occurs, run the following code block usually to display the error message
         if ui:
-            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
+
+            ui.messageBox('Failed:\n{}'.format(traceback.format_exc())) # Display a message box with the error message
