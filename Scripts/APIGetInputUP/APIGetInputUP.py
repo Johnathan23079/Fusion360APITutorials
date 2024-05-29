4b3ae6b431bfc8f5b88ffbae8a09d0a6f18d2809
Johnathan23079 <95047376+Johnathan23079@users.noreply.github.com>
2024-05-28 22:40:48 -0400

    Update APIGetInputUP.py

--- Scripts/APIGetInputUP/APIGetInputUP.py
+++ Scripts/APIGetInputUP/APIGetInputUP.py
@@ -10,20 +10,20 @@ def run(context):
         ui  = app.userInterface
         design = adsk.fusion.Design.cast(app.activeProduct)
 
-        defaultVal = "5.0 mm"
-        newInput = ui.inputBox('enter a new input val','New Input val', defaultVal) 
-        if newInput[0]:
+        defaultVal = "5.0 mm" #create a default value to display
+        newInput = ui.inputBox('enter a new input val','New Input val', defaultVal) #input box within the UI, add default value inside
+        if newInput[0]:                        #ensure the default value is used if nothing is used
             (defaultVal, isCanceled) = newInput
-        if isCanceled:
+        if isCanceled:            #end the program if cancel button is pressed
             return
-        unitsMgr = design.unitsManager
+        unitsMgr = design.unitsManager #define fusion 360 built in unit manager to convert units
         try:
-            NewInputReal = unitsMgr.evaluateExpression(newInput)
-            isValid = True
+            NewInputReal = unitsMgr.evaluateExpression(newInput) # check if the units inputed are recognized 
+            isValid = True #pass if true
         except:
             ui.messageBox('newInput' + 'is not a valid expression', 'Invalid Expression', adsk.core.MessageBoxButtonTypes.OKButtonType, adsk.core.MessageBoxIconTypes.CriticalIconType) 
             isValid = False 
-        ui.messageBox('Input', + defaultVal + 'result' + NewInputReal)
+        ui.messageBox('Input', + defaultVal + 'result' + NewInputReal) # input new values for user parameter and display in textbox
     except:
         if ui:
             ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
