7f3f7eb6089121112670dca017f29549720c946c
Johnathan23079 <95047376+Johnathan23079@users.noreply.github.com>
2024-05-28 22:46:36 -0400

    Update APIUserCreateUP.py

--- Scripts/APIUserCreateUP/APIUserCreateUP.py
+++ Scripts/APIUserCreateUP/APIUserCreateUP.py
@@ -1,5 +1,5 @@
-#Author-
-#Description-
+#Author-Johnathan Lee
+#Description-Here we will make a script to be able to create a user parameter in fusion based on user input
 
 import adsk.core, adsk.fusion, adsk.cam, traceback
 
@@ -10,17 +10,18 @@ def run(context):
         ui  = app.userInterface
         design = adsk.fusion.Design.cast(app.activeProduct)
 
-        defaultInput = 'diameter'
+        defaultInput = 'diameter'       #default values
         defaultInputNum =  '5 mm'
-        newInputName = ui.inputBox('Enter a new parameter name', 'New Parameter', defaultInput)
-        newInputValue = ui.inputBox('Enter a new parameter value', 'New Parameter', defaultInputNum)
+        newInputName = ui.inputBox('Enter a new parameter name', 'New Parameter', defaultInput)    #input box name
+        newInputValue = ui.inputBox('Enter a new parameter value', 'New Parameter', defaultInputNum) # input box value
 
-        unitsMgr = design.unitsManager
-        realInputNumber = unitsMgr.evaluateExpression(newInputValue[0], unitsMgr.defaultLengthUnits)
+        unitsMgr = design.unitsManager #begin the units manager
+        realInputNumber = unitsMgr.evaluateExpression(newInputValue[0], unitsMgr.defaultLengthUnits)# evaluate the expression in the input value and set it to default units wiht unit manager 
+                                                        #[] are used because it is 5 mm and 5 is in the [0[ slot this example only takes mm
+                                                        # a second input could be used for units if you want variability
+        realValueInput = adsk.core.ValueInput.createByReal(realInputNumber) #assign this value to the real input value
 
-        realValueInput = adsk.core.ValueInput.createByReal(realInputNumber)
-
-        design.userParameters.add(newInputName[0], realValueInput, unitsMgr.defaultLengthUnits, '')
+        design.userParameters.add(newInputName[0], realValueInput, unitsMgr.defaultLengthUnits, '')#create the parameter using the values
 
 
 
