9565c29df4fbb5d21d3ed9597c609be8d8e0f84d
Johnathan23079 <95047376+Johnathan23079@users.noreply.github.com>
2024-05-30 14:29:31 -0400

    Update Objects.py

--- Classes/Objects.py
+++ Classes/Objects.py
@@ -2,9 +2,15 @@
 #object = ExtrudeFeature
 #Application(Fuison) - UI - Design? - Solid - Extrudefeature
 #sketch - sketch line
-class LEAD:
+
+class LEAD: #name of the class
+    #create a function starting with self and two objects, first and second
     def __init__(self, first, second):
-        self.first = first
-        self.second = second
-apiSeries = (LEAD)
-formsSeries = (LEAD) 
+        self.first = first #self is at the top of the hierarchy and first is a sub-object
+        self.second = second #second is also a sub-object of self
+apiSeries = LEAD("first", "second") #this calls 
+formsSeries = LEAD("third", "fourth")
+print(apiSeries.first)
+print(apiSeries.second)
+
+
