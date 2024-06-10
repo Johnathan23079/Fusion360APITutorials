61e8950c0d20486f5e9d2d280149626fbb11576e
Johnathan23079 <95047376+Johnathan23079@users.noreply.github.com>
2024-06-09 22:15:45 -0400

    Update APIGetInputUP.py

--- Scripts/APIGetInputUP/APIGetInputUP.py
+++ Scripts/APIGetInputUP/APIGetInputUP.py
@@ -10,10 +10,10 @@ def run(context):
         ui  = app.userInterface
         design = adsk.fusion.Design.cast(app.activeProduct)
 
-        defaultVal = "5.0 mm" #create a default value to display
-        newInput = ui.inputBox('enter a new input val','New Input val', defaultVal) #input box within the UI, add default value inside
+        defaultInput = "5.0 mm" #create a default value to display
+        newInput = ui.inputBox('enter a new input val','New Input val', defaultInput) #input box within the UI, add default value inside
         if newInput[0]:                        #ensure the default value is used if nothing is used
-            (defaultVal, isCanceled) = newInput
+            (defaultInput, isCanceled) = newInput
         if isCanceled:            #end the program if cancel button is pressed
             return
         unitsMgr = design.unitsManager #define fusion 360 built in unit manager to convert units
@@ -23,7 +23,7 @@ def run(context):
         except:
             ui.messageBox('newInput' + 'is not a valid expression', 'Invalid Expression', adsk.core.MessageBoxButtonTypes.OKButtonType, adsk.core.MessageBoxIconTypes.CriticalIconType) 
             isValid = False 
-        ui.messageBox('Input', + defaultVal + 'result' + NewInputReal) # input new values for user parameter and display in textbox
+        ui.messageBox('Input: ' + defaultInput + ' result: ' + NewInputReal) # input new values for user parameter and display in textbox
     except:
         if ui:
             ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
